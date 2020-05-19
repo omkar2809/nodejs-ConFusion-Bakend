232c8ce1c67c3f3f6d310dbf265e618af13c51f0
omkar2809 <55548995+omkar2809@users.noreply.github.com>
2020-05-19 14:02:43 +0530

    OAuth passport facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '696325617605833', 
+        clientSecret: '857feaec25cd4cebac0c731b1da0c7b4'
+    }
 }
